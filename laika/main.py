5ea220630895214876f14c4a0668fef3dbd69489
Luiz Menezes <luiz.menezesf@gmail.com>
2026-04-21 21:04:02 -0300

    Add space api endpoint

--- laika/main.py
+++ laika/main.py
@@ -6,3 +6,26 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"Hello": "World"}
+
+
+@app.get("/status/space-api")
+def status_space_api():
+    return {
+        "space": "Sanca Hackerspace",
+        "api_compatibility": ["15"],
+        "logo": "",
+        "url": "https://hackerspace.sanca.log.br",
+        "location": {
+            "address": "",
+            "lat": "",
+            "lon": "",
+        },
+        "state": {
+            "open": False,
+        },
+        "contact": {
+            "telegram": "@sanca_hs"
+        },
+        "issue_report_channels": ["telegram"],
+        "projects": ["https://github.com/sancahs"],
+    }
